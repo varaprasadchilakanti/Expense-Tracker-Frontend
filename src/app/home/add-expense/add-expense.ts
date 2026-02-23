7081b8a046b93297e91446d9522f35b4636b09eb
Lenka Yaswith Krishna Vamsi <yaswith.lykv@gmail.com>
2026-02-23 23:15:54 +0530

    Updated Expense

--- src/app/home/add-expense/add-expense.ts
+++ src/app/home/add-expense/add-expense.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { HomeService } from '../home-service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-expense',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './add-expense.html',
   styleUrl: './add-expense.css',
 })
 export class AddExpense {
+  form = new FormGroup({
+    title: new FormControl(''),
+    amount: new FormControl(''),
+    category: new FormControl(''),
+    date: new FormControl('')
+  })
 
+  constructor(private homeService: HomeService, private router: Router){}
+
+  onSubmit(){
+    this.homeService.postExpenses(this.form.value).subscribe({
+      next: (response: any) =>{
+        console.log(response);
+        this.router.navigate(['']);
+      }
+    })
+    
+  }
 }
